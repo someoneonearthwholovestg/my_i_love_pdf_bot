8fdf359adcc3c6867d036e12a6ce76df8fa74dbb
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-05-03 16:53:48 +0530

    Update id.py

--- plugins/dm/id.py
+++ plugins/dm/id.py
@@ -2,35 +2,13 @@
 # copyright ©️ 2021 nabilanavab
 
 from pyrogram import filters
-from Configs.dm import Config
 from pyrogram import Client as ILovePDF
-from pyrogram.types import InlineKeyboardButton
-from pyrogram.types import InlineKeyboardMarkup
-
-#--------------->
-#--------> Config var.
-#------------------->
-
-BANNED_USERS=Config.BANNED_USERS
-ADMIN_ONLY=Config.ADMIN_ONLY
-ADMINS=Config.ADMINS
 
 #--------------->
 #--------> LOCAL VARIABLES
 #------------------->
 
-UCantUse = "For Some Reason You Can't Use This Bot 🛑"
-
-button=InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    "😉 Create your Own 😉",
-                    url="https://github.com/nabilanavab/ilovepdf"
-                )
-            ]
-       ]
-    )
+feedbackMsg = "[Write a feedback 📋](https://t.me/ilovepdf_bot)"
 
 #--------------->
 #--------> GET USER ID (/id)
@@ -39,15 +17,23 @@ button=InlineKeyboardMarkup(
 @ILovePDF.on_message(filters.private & ~filters.edited & filters.command(["id"]))
 async def userId(bot, message):
     try:
-        await message.reply_chat_action("typing")
-        if (message.chat.id in BANNED_USERS) or (
-            (ADMIN_ONLY) and (message.chat.id not in ADMINS)
-        ):
-            await message.reply_text(UCantUse, reply_markup=button)
-            return
-        await message.reply_text(
-            f'Your Id: `{message.chat.id}`', quote=True
-        )
+        await message.reply_text(f'Your Id: `{message.chat.id}`', quote=True)
+    except Exception:
+        pass
+
+
+@ILovePDF.on_message(filters.private & ~filters.edited & filters.command(["message"]))
+async def _cancelI2P(bot, message):
+    try:
+        await message.reply_text(f"message_id: `{message.message_id}` 🎭", quote=True)
+    except Exception:
+        pass
+
+
+@ILovePDF.on_message(filters.private & filters.command(["feedback"]) & ~filters.edited)
+async def feedback(bot, message):
+    try:
+        await message.reply_text(feedbackMsg, disable_web_page_preview=True)
     except Exception:
         pass
 
