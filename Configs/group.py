6953268c5896104f5a65b8c5f32aeae8d03323d6
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-05-03 17:16:49 +0530

    Create group.py

---
+++ Configs/group.py
@@ -0,0 +1,20 @@
+# fileName: Configs/group.py
+# copyright ©️ 2021 nabilanavab
+
+import os
+
+#--------------->
+#--------> CONFIG VAR.
+#------------------->
+
+class groupConfig(object):
+    
+    # add admins Id list by space seperated (Optional)
+    ONLY_GROUP=list(set(int(x) for x in os.environ.get("ONLY_GROUP", "0").split()))
+    
+    # banned groups can't use this bot (Optional)
+    BANNED_GROUP=list(set(int(x) for x in os.environ.get("BANNED_USERS", "0").split()))
+    if not BANNED_GROUP:
+        BANNED_GROUP=[]
+
+#                                                                             Telegram: @nabilanavab
