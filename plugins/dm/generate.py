244dff6a710e63cbb8151b8f2fdbcf40b0bc7aaa
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-05-03 16:54:45 +0530

    Update generate.py

--- plugins/dm/generate.py
+++ plugins/dm/generate.py
@@ -5,39 +5,15 @@ import os
 import shutil
 import asyncio
 from pdf import PDF
-from time import sleep
 from pyrogram import filters
 from Configs.dm import Config
 from pyrogram import Client as ILovePDF
-from pyrogram.types import InlineKeyboardButton
-from pyrogram.types import InlineKeyboardMarkup
-
-#--------------->
-#--------> Config var.
-#------------------->
-
-BANNED_USERS=Config.BANNED_USERS
-ADMIN_ONLY=Config.ADMIN_ONLY
-ADMINS=Config.ADMINS
 
 #--------------->
 #--------> LOCAL VARIABLES
 #------------------->
 
-UCantUse = "For Some Reason You Can't Use This Bot 🛑"
-
-feedbackMsg = "[Write a feedback 📋](https://t.me/nabilanavabchannel/17?comment=10)"
-
-button=InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    "😉 Create your Own 😉",
-                    url="https://github.com/nabilanavab/ilovepdf"
-                )
-            ]
-       ]
-    )
+feedbackMsg = "[Write a feedback 📋](https://t.me/ILovePDF_bot)"
 
 #--------------->
 #--------> REPLY TO /generate MESSAGE
@@ -46,68 +22,47 @@ button=InlineKeyboardMarkup(
 @ILovePDF.on_message(filters.private & filters.command(["generate"]) & ~filters.edited)
 async def generate(bot, message):
     try:
-        if (message.chat.id in BANNED_USERS) or (
-            (ADMIN_ONLY) and (message.chat.id not in ADMINS)
-        ):
-            await message.reply_text(
-                UCantUse,
-                reply_markup=button
-            )
-            return
-        
         # newName : new file name(/generate ___)
-        newName = str(message.text.replace("/generate", ""))
-        images = PDF.get(message.chat.id)
-        
+        newName=str(message.text.replace("/generate", ""))
+        images=PDF.get(message.chat.id)
         if isinstance(images, list):
-            pgnmbr = len(PDF[message.chat.id])
+            pgnmbr=len(PDF[message.chat.id])
             del PDF[message.chat.id]
         
         # IF NO IMAGES SEND BEFORE
         if not images:
             await message.reply_chat_action("typing")
-            imagesNotFounded = await message.reply_text(
-                "`No image founded.!!`😒"
-            )
-            sleep(5)
+            imagesNotFounded=await message.reply_text("`No image founded.!!`😒")
+            await asyncio.sleep(5)
             await message.delete()
             await imagesNotFounded.delete()
             return
+        gnrtMsgId=await message.reply_text(f"`Generating pdf..`💚")
         
-        gnrtMsgId = await message.reply_text(
-            f"`Generating pdf..`💚"
-        )
-        
-        if newName == " name":
-            fileName = f"{message.from_user.first_name}" + ".pdf"
+        if newName==" name":
+            fileName=f"{message.from_user.first_name}"+".pdf"
         elif len(newName) > 1 and len(newName) <= 45:
-            fileName = f"{newName}" + ".pdf"
+            fileName=f"{newName}"+".pdf"
         elif len(newName) > 45:
-            fileName = f"{message.from_user.first_name}" + ".pdf"
+            fileName=f"{message.from_user.first_name}"+".pdf"
         else:
-            fileName = f"{message.chat.id}" + ".pdf"
+            fileName=f"{message.chat.id}"+".pdf"
         
-        images[0].save(fileName, save_all = True, append_images = images[1:])
-        await gnrtMsgId.edit(
-            "`Uploading pdf.. `🏋️",
-        )
+        images[0].save(fileName, save_all=True, append_images=images[1:])
+        await gnrtMsgId.edit("`Uploading pdf.. `🏋️")
         await message.reply_chat_action("upload_document")
-        generated = await bot.send_document(
+        generated=await bot.send_document(
             chat_id=message.chat.id,
             document=open(fileName, "rb"),
             thumb=Config.PDF_THUMBNAIL,
             caption=f"file Name: `{fileName}`\n\n`Total pg's: {pgnmbr}`"
         )
-        await gnrtMsgId.edit(
-            "`Successfully Uploaded.. `🤫",
-        )
+        await gnrtMsgId.edit("`Successfully Uploaded.. `🤫")
         os.remove(fileName)
         shutil.rmtree(f"{message.chat.id}")
         await asyncio.sleep(5)
         await message.reply_chat_action("typing")
-        await message.reply_text(
-            feedbackMsg, disable_web_page_preview = True
-        )
+        await message.reply_text(feedbackMsg, disable_web_page_preview=True)
     except Exception:
         try:
             os.remove(fileName)
