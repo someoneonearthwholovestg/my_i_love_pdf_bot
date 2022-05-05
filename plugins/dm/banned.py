8fb44a94bf901bc5e09c0c5d8be31c3480a9efb5
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-05-05 08:22:19 +0530

    Update banned.py

--- plugins/dm/banned.py
+++ plugins/dm/banned.py
@@ -8,6 +8,7 @@ from Configs.group import groupConfig
 from pyrogram import Client as ILovePDF
 from pyrogram.types import InlineKeyboardButton
 from pyrogram.types import InlineKeyboardMarkup
+from Configs.db import BANNED_USR_DB, BANNED_GRP_DB
 
 #--------------->
 #--------> config vars
