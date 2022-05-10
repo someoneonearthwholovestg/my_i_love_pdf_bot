3af3f1d395eb3bcdbc1e128d11cdbec1fc521ad4
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-05-11 00:06:25 +0530

    Update compress.py

--- plugins/dm/callBack/compress.py
+++ plugins/dm/callBack/compress.py
@@ -45,7 +45,7 @@ async def compressPDF(message, message_id):
         ratio=(1-(compressedSize/initialSize))*100
         
         # sends only if compressed more than 10mb or ratio >= 5%
-        if compressedSize>1000000 or ratio>=5:
+        if True:#compressedSize>1000000 or ratio>=5:
             await message.edit("hi")
             return "compressedCaption" #.format(
                    # await gSF(initialSize), await gSF(compressedSize), ratio
