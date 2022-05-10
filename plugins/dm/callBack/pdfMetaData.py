9a16492863c83eaf8cdd4927587ca70632dfa73c
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-05-10 22:56:40 +0530

    Update pdfMetaData.py

--- plugins/dm/callBack/pdfMetaData.py
+++ plugins/dm/callBack/pdfMetaData.py
@@ -7,6 +7,7 @@ import shutil
 from pdf import PROCESS
 from pyrogram import filters
 from plugins.progress import progress
+from plugins.toKnown import knownButton
 from pyrogram import Client as ILovePDF
 from plugins.fileSize import get_size_format as gSF
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
@@ -33,34 +34,32 @@ File Size: `{}`
 #--------> PDF META DATA
 #------------------->
 
-pdfInfo = filters.create(lambda _, __, query: query.data == "pdfInfo")
-KpdfInfo = filters.create(lambda _, __, query: query.data.startswith("KpdfInfo"))
+pdfInfo=filters.create(lambda _, __, query: query.data=="pdfInfo")
 
 @ILovePDF.on_callback_query(pdfInfo)
 async def _pdfInfo(bot, callbackQuery):
     try:
+        chat_id=callbackQuery.message.chat.id
+        message_id=callbackQuery.message.message_id
+        
         # CHECKS PROCESS
-        if callbackQuery.message.chat.id in PROCESS:
-            await callbackQuery.answer(
-                "Work in progress.. 🙇"
-            )
+        if chat_id in PROCESS:
+            await callbackQuery.answer("Work in progress.. 🙇")
             return
+        
         # CB MESSAGE DELETES IF USER DELETED PDF
         try:
             fileExist=callbackQuery.message.reply_to_message.document.file_id
         except Exception:
-            await bot.delete_messages(
-                chat_id=callbackQuery.message.chat.id,
-                message_ids=callbackQuery.message.message_id
-            )
+            await callbackQuery.message.delete()
             return
+        
         # ADD TO PROCESS
-        PROCESS.append(callbackQuery.message.chat.id)
+        PROCESS.append(chat_id)
+        
         # DOWNLOADING STARTED
-        downloadMessage=await callbackQuery.edit_message_text(
-            "`Downloding your pdf..`⏳",
-        )
-        pdf_path=f"{callbackQuery.message.message_id}/pdfInfo.pdf"
+        downloadMessage=await callbackQuery.edit_message_text("`Downloding your pdf..`⏳", quote=True)
+        pdf_path=f"{message_id}/pdfInfo.pdf"
         file_id=callbackQuery.message.reply_to_message.document.file_id
         fileSize=callbackQuery.message.reply_to_message.document.file_size
         # DOWNLOAD PROGRESS
@@ -77,58 +76,27 @@ async def _pdfInfo(bot, callbackQuery):
         )
         # CHECKS IS DOWNLOADING COMPLETED OR PROCESS CANCELED
         if downloadLoc is None:
-            PROCESS.remove(callbackQuery.message.chat.id)
+            PROCESS.remove(chat_id)
             return
+        
         # OPEN FILE WITH FITZ
         with fitz.open(pdf_path) as pdf:
-            isPdf=pdf.is_pdf
-            metaData=pdf.metadata
-            isEncrypted=pdf.is_encrypted
-            number_of_pages=pdf.pageCount
+            isPdf=pdf.is_pdf; metaData=pdf.metadata
+            isEncrypted=pdf.is_encrypted; number_of_pages=pdf.pageCount
             # CHECKS IF FILE ENCRYPTED
             if isPdf and isEncrypted:
                 pdfMetaData=f"\nFile Encrypted 🔐\n"
             if isPdf and not(isEncrypted):
                 pdfMetaData="\n"
             # ADD META DATA TO pdfMetaData STRING
-            if metaData != None:
+            if metaData!=None:
                 for i in metaData:
-                    if metaData[i] != "":
-                        pdfMetaData += f"`{i}: {metaData[i]}`\n"
-            fileName = callbackQuery.message.reply_to_message.document.file_name
-            fileSize = callbackQuery.message.reply_to_message.document.file_size
+                    if metaData[i]!="":
+                        pdfMetaData+=f"`{i}: {metaData[i]}`\n"
+            fileName=callbackQuery.message.reply_to_message.document.file_name
+            fileSize=callbackQuery.message.reply_to_message.document.file_size
             if isPdf and not(isEncrypted):
-                editedPdfReplyCb=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton("⭐ META£ATA ⭐", callback_data=f"KpdfInfo|{number_of_pages}"),
-                            InlineKeyboardButton("🗳️ PREVIEW 🗳️", callback_data=f"Kpreview"),
-                        ],[
-                            InlineKeyboardButton("🖼️ toIMAGES 🖼️", callback_data=f"KtoImage|{number_of_pages}"),
-                            InlineKeyboardButton("✏️ toTEXT ✏️", callback_data=f"KtoText|{number_of_pages}")
-                        ],[
-                            InlineKeyboardButton("🔐 ENCRYPT 🔐",callback_data=f"Kencrypt|{number_of_pages}"),
-                            InlineKeyboardButton("🔒 DECRYPT 🔓", callback_data=f"notEncrypted")
-                        ],[
-                            InlineKeyboardButton("🗜️ COMPRESS 🗜️", callback_data=f"Kcompress"),
-                            InlineKeyboardButton("🤸 ROTATE 🤸", callback_data=f"Krotate|{number_of_pages}")
-                        ],[
-                            InlineKeyboardButton("✂️ SPLIT ✂️", callback_data=f"Ksplit|{number_of_pages}"),
-                            InlineKeyboardButton("🧬 MERGE 🧬", callback_data="merge")
-                        ],[
-                            InlineKeyboardButton("™️ STAMP ™️", callback_data=f"Kstamp|{number_of_pages}"),
-                            InlineKeyboardButton("✏️ RENAME ✏️", callback_data="rename")
-                        ],[
-                            InlineKeyboardButton("📝 OCR 📝", callback_data=f"Kocr|{number_of_pages}"),
-                            InlineKeyboardButton("🥷 A4 FORMAT 🥷", callback_data=f"Kformat|{number_of_pages}")
-                        ],[
-                            InlineKeyboardButton("🤐 ZIP 🤐", callback_data=f"Kzip|{number_of_pages}"),
-                            InlineKeyboardButton("🎯 TAR 🎯", callback_data=f"Ktar|{number_of_pages}")
-                        ],[
-                            InlineKeyboardButton("🚫 CLOSE 🚫", callback_data="closeALL")
-                        ]
-                    ]
-                )
+                editedPdfReplyCb=knownButton
                 await callbackQuery.edit_message_text(
                     pdfInfoMsg.format(
                         fileName, await gSF(fileSize), number_of_pages
@@ -141,48 +109,31 @@ async def _pdfInfo(bot, callbackQuery):
                         fileName, await gSF(fileSize), number_of_pages
                     ) + pdfMetaData,
                     reply_markup=InlineKeyboardMarkup(
-                        [
-                            [
-                                InlineKeyboardButton("🔓 DECRYPT 🔓", callback_data="decrypt")
-                            ],[
-                                InlineKeyboardButton("🚫 CLOSE 🚫", callback_data="closeALL")
-                            ]
-                        ]
+                        [[
+                            InlineKeyboardButton("🔓 DECRYPT 🔓", callback_data="decrypt")
+                        ],[
+                            InlineKeyboardButton("🚫 CLOSE 🚫", callback_data="closeALL")
+                        ]]
                     )
                 )
-            PROCESS.remove(callbackQuery.message.chat.id)
-            shutil.rmtree(f"{callbackQuery.message.message_id}")
+            PROCESS.remove(chat_id)
+            shutil.rmtree(f"{message_id}")
     # EXCEPTION DURING FILE OPENING
     except Exception as e:
         try:
             await callbackQuery.edit_message_text(
                 f"SOMETHING went WRONG.. 🐉\n\nERROR: {e}",
                 reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton("❌ Error in file ❌", callback_data = f"error")
-                        ],[
-                            InlineKeyboardButton("🚫 CLOSE 🚫", callback_data="closeALL")
-                        ]
-                    ]
+                    [[
+                        InlineKeyboardButton("❌ Error in file ❌", callback_data = f"error")
+                    ],[
+                        InlineKeyboardButton("🚫 CLOSE 🚫", callback_data="closeALL")
+                    ]]
                 )
             )
-            PROCESS.remove(callbackQuery.message.chat.id)
-            shutil.rmtree(f"{callbackQuery.message.message_id}")
+            PROCESS.remove(chat_id)
+            shutil.rmtree(f"{message_id}")
         except Exception:
             pass
-
-@ILovePDF.on_callback_query(KpdfInfo)
-async def _KpdfInfo(bot, callbackQuery):
-    try:
-        _, number_of_pages = callbackQuery.data.split("|")
-        await bot.answer_callback_query(
-            callbackQuery.id,
-            text = f"Total {number_of_pages} pages 😉",
-            show_alert = True,
-            cache_time = 0
-        )
-    except Exception:
-        pass
 
 #                                                                                              Telegram: @nabilanavab
