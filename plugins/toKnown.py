a39f6a24daf76de8318aae076046b41c944d3381
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-05-13 00:46:26 +0530

    Update toKnown.py

--- plugins/toKnown.py
+++ plugins/toKnown.py
@@ -53,9 +53,6 @@ async def toKnown(callbackQuery, number_of_pages):
                     InlineKeyboardButton("📝 OCR 📝", callback_data=f"Kocr|{number_of_pages}"),
                     InlineKeyboardButton("🥷 A4 FORMAT 🥷", callback_data=f"Kformat|{number_of_pages}")
                 ],[
-                    InlineKeyboardButton("🤐 ZIP 🤐", callback_data=f"Kzip|{number_of_pages}"),
-                    InlineKeyboardButton("🎯 TAR 🎯", callback_data=f"Ktar|{number_of_pages}")
-                ],[
                     InlineKeyboardButton("🚫 CLOSE 🚫", callback_data="closeALL")
                 ]]
             )
