0870a971e124843a3168d66d080aeb88256382c7
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-05-13 16:39:29 +0530

    Update txt2pdf.py

--- plugins/dm/txt2pdf.py
+++ plugins/dm/txt2pdf.py
@@ -5,22 +5,12 @@ import os
 from fpdf import FPDF
 from pdf import PROCESS
 from pyrogram import filters
-from Configs.dm import Config
 from pyrogram import Client as ILovePDF
+from configs.images import PDF_THUMBNAIL
 from pyrogram.types import InlineKeyboardButton
 from pyrogram.types import InlineKeyboardMarkup
 
-#--------------->
-#--------> config vars
-#------------------->
-
-PDF_THUMBNAIL=Config.PDF_THUMBNAIL
-
-#--------------->
-#--------> LOCAL VARIABLES
-#------------------->
-
-TXT = {}
+TXT={}
 
 #--------------->
 #--------> REPLY TO /txt2pdf
@@ -33,49 +23,45 @@ async def feedback(bot, message):
         await message.reply_text(
             text="__Now, Please Select A Font Style »__",
             reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton("Times", callback_data="font|t"),
-                        InlineKeyboardButton("Courier", callback_data="font|c")
-                    ],[
-                        InlineKeyboardButton("Helvetica (Default)", callback_data="font|h")
-                    ],[
-                        InlineKeyboardButton("Symbol", callback_data="font|s"),
-                        InlineKeyboardButton("Zapfdingbats", callback_data="font|z")
-                    ],[
-                        InlineKeyboardButton("🚫 €lose ", callback_data="closeme")
-                    ]
-                ]
+                [[
+                    InlineKeyboardButton("Times", callback_data="font|t"),
+                    InlineKeyboardButton("Courier", callback_data="font|c")
+                ],[
+                    InlineKeyboardButton("Helvetica (Default)", callback_data="font|h")
+                ],[
+                    InlineKeyboardButton("Symbol", callback_data="font|s"),
+                    InlineKeyboardButton("Zapfdingbats", callback_data="font|z")
+                ],[
+                    InlineKeyboardButton("🚫 €lose ", callback_data="closeme")
+                ]]
             )
         )
         await message.delete()
     except Exception as e:
         print(e)
 
-txt2pdf = filters.create(lambda _, __, query: query.data.startswith("font"))
+txt2pdf=filters.create(lambda _, __, query: query.data.startswith("font"))
 
 @ILovePDF.on_callback_query(txt2pdf)
 async def _txt2pdf(bot, callbackQuery):
     try:
-        _, font = callbackQuery.data.split("|")
+        _, font=callbackQuery.data.split("|")
         await callbackQuery.message.edit(
             text=f"Text to Pdf» Now Select Page Size »",
             reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton("Portarate", callback_data=f"pgSize|{font}|p")
-                    ],[
-                        InlineKeyboardButton("Landscape", callback_data=f"pgSize|{font}|l")
-                    ],[
-                        InlineKeyboardButton("« Back «", callback_data=f"txt2pdfBack")
-                    ]
-                ]
+                [[
+                    InlineKeyboardButton("Portarate", callback_data=f"pgSize|{font}|p")
+                ],[
+                    InlineKeyboardButton("Landscape", callback_data=f"pgSize|{font}|l")
+                ],[
+                    InlineKeyboardButton("« Back «", callback_data=f"txt2pdfBack")
+                ]]
             )
         )
     except Exception as e:
         print(e)
 
-txt2pdfBack = filters.create(lambda _, __, query: query.data == "txt2pdfBack")
+txt2pdfBack=filters.create(lambda _, __, query: query.data == "txt2pdfBack")
 
 @ILovePDF.on_callback_query(txt2pdfBack)
 async def _txt2pdfBack(bot, callbackQuery):
@@ -83,101 +69,79 @@ async def _txt2pdfBack(bot, callbackQuery):
         await callbackQuery.message.edit(
             text="__Now, Please Select A Font Style »__",
             reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton("Times", callback_data="font|t"),
-                        InlineKeyboardButton("Courier", callback_data="font|c")
-                    ],[
-                        InlineKeyboardButton("Helvetica", callback_data="font|h")
-                    ],[
-                        InlineKeyboardButton("Symbol", callback_data="font|s"),
-                        InlineKeyboardButton("Zapfdingbats", callback_data="font|z")
-                    ],[
-                        InlineKeyboardButton("🚫 €lose ", callback_data="closeme")
-                    ]
-                ]
+                [[
+                    InlineKeyboardButton("Times", callback_data="font|t"),
+                    InlineKeyboardButton("Courier", callback_data="font|c")
+                ],[
+                    InlineKeyboardButton("Helvetica", callback_data="font|h")
+                ],[
+                    InlineKeyboardButton("Symbol", callback_data="font|s"),
+                    InlineKeyboardButton("Zapfdingbats", callback_data="font|z")
+                ],[
+                    InlineKeyboardButton("🚫 €lose ", callback_data="closeme")
+                ]]
             ),
             disable_web_page_preview=True
         )
     except Exception as e:
         print(e)
 
-pgSize = filters.create(lambda _, __, query: query.data.startswith("pgSize"))
+pgSize=filters.create(lambda _, __, query: query.data.startswith("pgSize"))
 
 @ILovePDF.on_callback_query(pgSize)
 async def _pgSize(bot, callbackQuery):
     try:
-        if callbackQuery.message.chat.id in PROCESS:
-            await callbackQuery.answer(
-                "Work in progress.. 🙇"
-            )
+        chat_id=callbackQuery.message.chat.id
+        message_id=callbackQuery.message.message_id
+        if chat_id in PROCESS:
+            await callbackQuery.answer("Work in progress.. 🙇")
             return
-        bla, _, __ = callbackQuery.data.split("|")
-        PROCESS.append(callbackQuery.message.chat.id)
-        TXT[callbackQuery.message.chat.id] = []
+        bla, _, __=callbackQuery.data.split("|")
+        PROCESS.append(chat_id); TXT[chat_id]=[]
         nabilanavab=True
         while(nabilanavab):
             # 1st value will be pdf title
-            askPDF = await bot.ask(
+            askPDF=await bot.ask(
                 text="__TEXT TO PDF » Now, please enter a TITLE:__\n\n/exit __to cancel__\n/skip __to skip__",
-                chat_id=callbackQuery.message.chat.id,
-                reply_to_message_id=callbackQuery.message.message_id,
-                filters=None
+                chat_id=chat_id, reply_to_message_id=message_id, filters=None
             )
-            if askPDF.text == "/exit":
-                await bot.send_message(
-                    callbackQuery.message.chat.id,
-                    "`Process Cancelled..` 😏"
-                )
-                PROCESS.remove(callbackQuery.message.chat.id)
-                del TXT[callbackQuery.message.chat.id]
+            if askPDF.text=="/exit":
+                await askPDF.reply("`Process Cancelled..` 😏", quote=True)
+                PROCESS.remove(chat_id); del TXT[chat_id]
                 break
-            elif askPDF.text == "/skip":
-                TXT[callbackQuery.message.chat.id].append(None)
-                nabilanavab=False
+            elif askPDF.text=="/skip":
+                TXT[chat_id].append(None); nabilanavab=False
             elif askPDF.text:
-                TXT[callbackQuery.message.chat.id].append(f"{askPDF.text}")
-                nabilanavab=False
+                TXT[chat_id].append(f"{askPDF.text}"); nabilanavab=False
         # nabilanavab=True ONLY IF PROCESS CANCELLED
-        if nabilanavab == True:
-            PROCESS.remove(callbackQuery.message.chat.id)
-            TXT.remove(callbackQuery.message.chat.id)
+        if nabilanavab==True:
+            PROCESS.remove(chat_id); TXT.remove(chat_id)
             return
         nabilanavab=True
         while(nabilanavab):
             # other value will be pdf para
-            askPDF = await bot.ask(
-                text=f"__TEXT TO PDF » Now, please enter paragraph {len(TXT[callbackQuery.message.chat.id])-1}:__"
+            askPDF=await bot.ask(
+                text=f"__TEXT TO PDF » Now, please enter paragraph {len(TXT[chat_id])-1}:__"
                       "\n\n/exit __to cancel__\n/create __to create__",
-                chat_id=callbackQuery.message.chat.id,
-                reply_to_message_id=callbackQuery.message.message_id,
-                filters=None
+                chat_id=chat_id, reply_to_message_id=message_id, filters=None
             )
-            if askPDF.text == "/exit":
-                await bot.send_message(
-                    callbackQuery.message.chat.id,
-                    "`Process Cancelled..` 😏"
-                )
-                PROCESS.remove(callbackQuery.message.chat.id)
-                del TXT[callbackQuery.message.chat.id]
+            if askPDF.text=="/exit":
+                await askPDF.reply("`Process Cancelled..` 😏", quote=True)
+                PROCESS.remove(chat_id); del TXT[chat_id]
                 break
-            elif askPDF.text == "/create":
-                if TXT[callbackQuery.message.chat.id][0]==None and len(TXT[callbackQuery.message.chat.id])==1:
-                    await bot.send_message(
-                        callbackQuery.message.chat.id,
-                        "Nothing to create.. 😏"
-                    )
+            elif askPDF.text=="/create":
+                if TXT[chat_id][0]==None and len(TXT[chat_id])==1:
+                    await askPDF.reply("Nothing to create.. 😏", quote=True)
                 else:
-                    processMessage = await callbackQuery.message.reply_text(
+                    processMessage=await askPDF.reply(
                         "Started Converting txt to Pdf..🎉", quote=True
                     )
                     nabilanavab=False
             elif askPDF.text:
-                TXT[callbackQuery.message.chat.id].append(f"{askPDF.text}")
+                TXT[chat_id].append(f"{askPDF.text}")
         # nabilanavab=True ONLY IF PROCESS CANCELLED
-        if nabilanavab == True:
-            PROCESS.remove(callbackQuery.message.chat.id)
-            TXT.remove(callbackQuery.message.chat.id)
+        if nabilanavab==True:
+            PROCESS.remove(chat_id); TXT.remove(chat_id)
             return
         
         # Started Creating PDF
@@ -192,34 +156,27 @@ async def _pgSize(bot, callbackQuery):
         elif _ == "z":
             font="ZapfDingbats"
         
-        pdf = FPDF()
-        pdf.add_page(orientation=__)
+        pdf=FPDF(); pdf.add_page(orientation=__)
         pdf.set_font(font, "B", size=20)
-        if TXT[callbackQuery.message.chat.id][0] != None:
-            pdf.cell(200, 20, txt=TXT[callbackQuery.message.chat.id][0], ln=1, align="C")
+        if TXT[chat_id][0]!=None:
+            pdf.cell(200, 20, txt=TXT[chat_id][0], ln=1, align="C")
         pdf.set_font(font, size=15)
-        for _ in TXT[callbackQuery.message.chat.id][1:]:
+        for _ in TXT[chat_id][1:]:
             pdf.multi_cell(200, 10, txt=_, border=0, align="L")
-        pdf.output(f"{callbackQuery.message.message_id}.pdf")
+        pdf.output(f"{message_id}.pdf")
         await callbackQuery.message.reply_chat_action("upload_document")
-        await processMessage.edit(
-            "`Started Uploading..` 🏋️"
-        )
+        await processMessage.edit("`Started Uploading..` 🏋️")
         await callbackQuery.message.reply_document(
             file_name="txt2.pdf", quote=True,
-            document=open(f"{callbackQuery.message.message_id}.pdf", "rb"),
+            document=open(f"{message_id}.pdf", "rb"),
             thumb=PDF_THUMBNAIL
         )
-        await processMessage.delete()
-        PROCESS.remove(callbackQuery.message.chat.id)
-        os.remove(f"{callbackQuery.message.message_id}")
-        TXT.remove(callbackQuery.message.chat.id)
+        await processMessage.delete(); PROCESS.remove(chat_id)
+        os.remove(f"{message_id}"); TXT.remove(chat_id)
     except Exception as e:
         try:
-            PROCESS.remove(callbackQuery.message.chat.id)
-            await processMessage.edit(f"`ERROR`: __{e}__")
-            os.remove(f"{callbackQuery.message.message_id}.pdf")
-            TXT.remove(callbackQuery.message.chat.id)
+            PROCESS.remove(chat_id); await processMessage.edit(f"`ERROR`: __{e}__")
+            os.remove(f"{message_id}.pdf"); TXT.remove(chat_id)
         except Exception:
             pass
 
