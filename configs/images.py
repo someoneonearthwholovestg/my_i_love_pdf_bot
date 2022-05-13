704ed101d78c48de8dfdb1291db662fd0922a759
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-05-13 16:33:40 +0530

    Create images.py

---
+++ configs/images.py
@@ -0,0 +1,34 @@
+# fileName: configs/images.py
+# copyright ©️ 2021 nabilanavab
+
+# from Configs.images import *
+
+
+# ❌ DEFAULT THUMBNAIL ❌
+
+# from Configs.images import PDF_THUMBNAIL
+# PDF_THUMBNAIL="./images/thumbnail.jpeg"
+PDF_THUMBNAIL="https://te.legra.ph/file/50c4d6e580ed98d931549.jpg"
+
+
+# ❌ WELCOME IMAGE ❌
+
+# from Configs.images import WELCOME_PIC
+# WELCOME_IMAGE="./images/start.jpeg"
+WELCOME_PIC="https://te.legra.ph/file/50c4d6e580ed98d931549.jpg"
+
+
+# ❌ BANNED IMAGE ❌
+
+# from Configs.images import BANNED_PIC
+# BANNED_MESSAGE="./images/banned.jpeg"
+BANNED_PIC="https://te.legra.ph/file/50c4d6e580ed98d931549.jpg"
+
+
+# ❌ BIG FILE ❌
+
+# from Configs.images import BIG_FILE
+# BIG_FILE="./images/bigFile.jpeg"
+BIG_FILE="https://te.legra.ph/file/50c4d6e580ed98d931549.jpg"
+
+#                                                             @nabilanavab
