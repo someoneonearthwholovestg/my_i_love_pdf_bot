f7e95cdbc40a5b2cccd3aad37fe0f19369f0aa2d
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-05-18 23:18:50 +0530

    Update banned.py

--- plugins/dm/banned.py
+++ plugins/dm/banned.py
@@ -7,6 +7,7 @@ from pyrogram import filters
 from configs.dm import Config
 from configs.db import isMONGOexist
 from pyrogram import Client as ILovePDF
+from pyrogram.errors import ChatAdminRequired
 from plugins.dm.admin import BANNED_USR_DB, BANNED_GRP_DB
 from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ChatAdminRequired
