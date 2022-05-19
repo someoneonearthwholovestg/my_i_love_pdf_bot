af14dd2d1210a521460733caab41cab597eeb373
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-05-19 07:21:17 +0530

    Update pdf.py

--- plugins/dm/callBack/pdf.py
+++ plugins/dm/callBack/pdf.py
@@ -5,9 +5,9 @@ import os
 import time
 import shutil
 import asyncio
+from pdf import PROCESS
 from pyromod import listen
 from pyrogram import filters
-from configs.global import PROCESS
 from plugins.checkPdf import checkPdf
 from plugins.progress import progress
 from pyrogram.types import ForceReply
