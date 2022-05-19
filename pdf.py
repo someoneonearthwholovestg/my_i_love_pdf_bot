09845d25c5403b567e4e846fee004538fb5dd3ef
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-05-19 06:56:00 +0530

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -99,7 +99,6 @@ import logging
 from pyromod import listen
 from configs.dm import Config
 from pyrogram import Client, idle
-from configs.global import GlobaL
 
 # LOGGING INFO: DEBUG
 logging.basicConfig(
