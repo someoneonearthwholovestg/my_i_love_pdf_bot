473f02249cc2a69037d37e52588f362f88f8767e
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-05-19 06:18:01 +0530

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -1,4 +1,4 @@
-# !/USR/BIN/PYTHON
+"""# !/USR/BIN/PYTHON
 # -*- COADING: UTF-8 -*-
 # COPYRIGHT ©️ 2021 NABILANAVAB
 
@@ -39,7 +39,7 @@ invite_link=None
 # PLUGIN DIRECTORY
 plugin=dict(root="plugins")
 
-"""
+
 class Bot(ILovePDF):
 
     def __init__(self):
@@ -64,7 +64,7 @@ class Bot(ILovePDF):
 app=Bot()
 app.run()
 
-"""
+
 # PYROGRAM BOT AUTHENTIFICATION
 bot=ILovePDF(
     "ILovePDF",
@@ -80,3 +80,53 @@ bot.start()
 idle()
 bot.stop()
 
+"""
+# !/USR/BIN/PYTHON
+# -*- COADING: UTF-8 -*-
+# COPYRIGHT ©️ 2021 NABILANAVAB
+
+'''
+  _   _                  ___  ___  ____ ™
+ | | | |   _____ _____  | _ \|   \|  __| 
+ | | | |__/ _ \ V / -_) |  _/| |) |  _|  
+ |_| |___,\___/\_/\___| |_|  |___/|_|    
+                         [Nabil A Navab] 
+                         Email: nabilanavab@gmail.com
+                         Telegram: @nabilanavab
+ '''
+
+import logging
+from pyromod import listen
+from configs.dm import Config
+from pyrogram import Client, idle
+
+# LOGGING INFO: DEBUG
+logging.basicConfig(
+    level = logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+
+# GLOBAL VARIABLES
+PDF = {}            # save images for generating pdf
+PROCESS = []        # to check current process
+invite_link = None
+
+# PLUGIN DIRECTORY
+plugin = dict(
+    root = "plugins"
+)
+
+# PYROGRAM BOT AUTHENTIFICATION
+bot = Client(
+    "ILovePDF",
+    plugins = plugin,
+    api_id = Config.API_ID,
+    parse_mode = "markdown",
+    api_hash = Config.API_HASH,
+    bot_token = Config.API_TOKEN
+)
+
+bot.start()
+idle()
+bot.stop()
