3025cc5cedd866fb4f43d502f1c69bc84ec60d57
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-05-22 03:39:22 +0530

    Update thumbName.py

--- plugins/thumbName.py
+++ plugins/thumbName.py
@@ -10,38 +10,44 @@ from configs.images import CUSTOM_THUMBNAIL_U
 if isMONGOexist:
     from database import db
 
-newName=False
-if DEFAULT_NAME:
-    newName=True
+# newName=False
+# if DEFAULT_NAME:
+#    newName=True
 
 # return thumbnail and fileName
 async def thumbName(userID, fileName):
-    
-    fileNm, fileExt=os.path.splitext(fileName)
-    if newName:
-        DEFAULT_NAME=DEFAULT_NAME+fileExt
-    
-    # if no mongoDB return False [default thumbnail ]
-    if not isMONGOexist:
-        # id no DEFAULT_NAME, use current file name 
-        if newName:
-            return PDF_THUMBNAIL, DEFAULT_NAME
+    try:
+        fileNm, fileExt=os.path.splitext(fileName)
+        if DEFAULT_NAME:
+            DEFAULT_NAME=DEFAULT_NAME+fileExt
+        
+        # if no mongoDB return False [default thumbnail ]
+        if not isMONGOexist:
+            # id no DEFAULT_NAME, use current file name 
+            if DEFAULT_NAME:
+                return PDF_THUMBNAIL, DEFAULT_NAME
+            else:
+                return PDF_THUMBNAIL, fileName
+        
+        # user with thumbnail
+        if userID in CUSTOM_THUMBNAIL_U:
+            thumbnail=await db.get_thumbnail(userID)
+            if DEFAULT_NAME:
+                return thumbnail, DEFAULT_NAME
+            else:
+                return thumbnail, fileName
+        
+        # user without thumbnail
         else:
-            return PDF_THUMBNAIL, fileName
+            if DEFAULT_NAME:
+                return PDF_THUMBNAIL, DEFAULT_NAME
+            else:
+                return PDF_THUMBNAIL, fileName
     
-    # user with thumbnail
-    if userID in CUSTOM_THUMBNAIL_U:
-        thumbnail=await db.get_thumbnail(userID)
-        if newName:
-            return thumbnail, DEFAULT_NAME
-        else:
-            return thumbnail, fileName
-    
-    # user without thumbnail
-    else:
-        if newName:
-            return PDF_THUMBNAIL, DEFAULT_NAME
-        else:
-            return PDF_THUMBNAIL, fileName
+    except Exception as e:
+        logger.exception(
+                        "NAN: %(message.chat.id)s cause %(e)s error",
+                        exc_info=True
+                        )
 
-#                                       Telegram: @nabilanavab
+#                                                   Telegram: @nabilanavab
