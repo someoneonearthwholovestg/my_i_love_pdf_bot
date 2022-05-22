c73e657b2cb5a9fd0dbd1a50281ba1392501d36a
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-05-23 01:22:23 +0530

    Update footer.py

--- plugins/footer.py
+++ plugins/footer.py
@@ -1,6 +1,14 @@
 # fileName : plugins/footer.py
 # copyright ©️ 2021 nabilanavab
 
+# LOGGING INFO: DEBUG
+import logging
+logger=logging.getLogger(__name__)
+logging.basicConfig(
+                   level=logging.DEBUG,
+                   format="%(levelname)s:%(name)s:%(message)s" # %(asctime)s:
+                   )
+
 from asyncio import sleep
 from pyrogram.types import Message
 from configs.db import LOG_CHANNEL
@@ -19,22 +27,25 @@ async def footer(message, file):
                            f"[Write a Feedback]({FEEDBACK})"
                            )
         if LOG_CHANNEL:
-            userINFO=await message.get_users()
-            banUserCB=InlineKeyboardMarkup(
+            userINFO = await message.get_users()
+            banUserCB = InlineKeyboardMarkup(
                    [[
                           InlineKeyboardButton(
                                     "B@N",
-                                    callback_data=f"banU|{message.chat.id}"
+                                    callback_data = f"banU|{message.chat.id}"
                           )
                    ]]
             )
             await file.copy(
-                           chat_id=LOG_CHANNEL,
-                           caption=f"__User Name:__ {userINFO.mention}"
-                                   f"__User ID:__ `{userINFO.id}`",
-                           reply_markup=banUserCB if isMONGOexist else None
+                           chat_id = LOG_CHANNEL,
+                           caption = f"__User Name:__ {userINFO.mention}"
+                                    f"__User ID:__ `{userINFO.id}`",
+                           reply_markup = banUserCB if isMONGOexist else None
                            )
-    except Exception:
-        pass
+    except Exception as e:
+        logger.exception(
+                        "FOOTER:CAUSES %(e)s ERROR",
+                        exc_info=True
+                        )
 
 #                                                                                  Telegram: @nabilanavab
