1ff1bf7a65ae5f1a3317d38068976aa5f207c597
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-05-23 01:23:59 +0530

    Update photo.py

--- plugins/dm/photo.py
+++ plugins/dm/photo.py
@@ -1,6 +1,14 @@
 # fileName : Plugins/dm/photo.py
 # copyright ©️ 2021 nabilanavab
 
+# LOGGING INFO: DEBUG
+import logging
+logger=logging.getLogger(__name__)
+logging.basicConfig(
+                   level=logging.DEBUG,
+                   format="%(levelname)s:%(name)s:%(message)s" # %(asctime)s:
+                   )
+
 import os
 from PIL import Image
 from pyrogram import filters
@@ -10,17 +18,17 @@ from pyrogram import Client as ILovePDF
 from configs.images import WELCOME_PIC, BANNED_PIC
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-UPDATE_CHANNEL=Config.UPDATE_CHANNEL
+UPDATE_CHANNEL = Config.UPDATE_CHANNEL
 
 #--------------->
 #--------> LOCAL VARIABLES
 #------------------->
 
-imageAdded="""`Added {} page/'s to your pdf..`🤓
+imageAdded = """`Added {} page/'s to your pdf..`🤓
 
 /generate to generate PDF 🤞"""
 
-forceSubMsg="""Wait [{}](tg://user?id={})..!!
+forceSubMsg = """Wait [{}](tg://user?id={})..!!
 
 Due To The Huge Traffic Only Channel Members Can Use this Bot 🚶
 
@@ -40,49 +48,54 @@ async def images(bot, message):
         # CHECK USER IN CHANNEL (IF UPDATE_CHANNEL ADDED)
         if UPDATE_CHANNEL:
             try:
-                userStatus=await bot.get_chat_member(str(UPDATE_CHANNEL), message.chat.id)
+                userStatus = await bot.get_chat_member(str(UPDATE_CHANNEL), message.chat.id)
                 # IF USER BANNED FROM CHANNEL
-                if userStatus.status=='banned':
+                if userStatus.status == 'banned':
                      await message.reply_photo(
-                         photo=BANNED_PIC, quote=True,
-                         caption="For Some Reason You Can't Use This Bot"
-                                 "\n\nContact Bot Owner 🤐",
-                         reply_markup=InlineKeyboardMarkup(
+                         photo = BANNED_PIC, quote = True,
+                         caption = "For Some Reason You Can't Use This Bot"
+                                   "\n\nContact Bot Owner 🤐",
+                         reply_markup = InlineKeyboardMarkup(
                             [[InlineKeyboardButton("Owner 🎊", url="https://t.me/nabilanavab")]]
                          )
                      )
                      return
             except Exception:
-                if invite_link==None:
-                    invite_link=await bot.create_chat_invite_link(int(UPDATE_CHANNEL))
+                if invite_link == None:
+                    invite_link = await bot.create_chat_invite_link(int(UPDATE_CHANNEL))
                 await message.reply_photo(
-                    photo=WELCOME_PIC, quote=True,
-                    caption=forceSubMsg.format(
+                    photo = WELCOME_PIC, quote = True,
+                    caption = forceSubMsg.format(
                         message.from_user.first_name, message.chat.id
                     ),
-                    reply_markup=InlineKeyboardMarkup(
+                    reply_markup = InlineKeyboardMarkup(
                         [[
-                            InlineKeyboardButton("🌟 JOIN CHANNEL 🌟", url=invite_link.invite_link)
+                            InlineKeyboardButton("🌟 JOIN CHANNEL 🌟",
+                                        url = invite_link.invite_link)
                         ],[
-                            InlineKeyboardButton("Refresh ♻️", callback_data="refreshImg")
+                            InlineKeyboardButton("Refresh ♻️",
+                                         callback_data = "refreshImg")
                         ]]
                     )
                 )
                 return
-        imageReply=await message.reply_text("`Downloading your Image..⏳`", quote=True)
+        imageReply = await message.reply_text("`Downloading your Image..⏳`", quote=True)
         if not isinstance(PDF.get(message.chat.id), list):
-            PDF[message.chat.id]=[]
+            PDF[message.chat.id] = []
         await message.download(
             f"{message.chat.id}/{message.chat.id}.jpg"
         )
-        img=Image.open(
+        img = Image.open(
             f"{message.chat.id}/{message.chat.id}.jpg"
         ).convert("RGB")
         PDF[message.chat.id].append(img)
         await imageReply.edit(
             imageAdded.format(len(PDF[message.chat.id]))
         )
-    except Exception:
-        pass
+    except Exception as e:
+        logger.exception(
+                        "PHOTO:CAUSES %(e)s ERROR",
+                        exc_info=True
+                        )
 
 #                                                                                  Telegram: @nabilanavab
