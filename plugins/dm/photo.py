2c61d2227ac52731c99df51bb8196965dfec1890
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-05-24 00:31:34 +0530

    Update photo.py

--- plugins/dm/photo.py
+++ plugins/dm/photo.py
@@ -40,7 +40,12 @@ hit on "retry ♻️" after joining.. 😅"""
 #--------> REPLY TO IMAGES
 #------------------->
 
-@ILovePDF.on_message(filters.private & ~filters.edited & filters.photo & filters.incoming)
+@ILovePDF.on_message(
+                    filters.photo &
+                    filters.private &
+                    ~filters.edited &
+                    filters.incoming
+                    )
 async def images(bot, message):
     try:
         global invite_link
@@ -48,49 +53,56 @@ async def images(bot, message):
         # CHECK USER IN CHANNEL (IF UPDATE_CHANNEL ADDED)
         if UPDATE_CHANNEL:
             try:
-                userStatus = await bot.get_chat_member(str(UPDATE_CHANNEL), message.chat.id)
+                userStatus = await bot.get_chat_member(
+                                                      str(UPDATE_CHANNEL),
+                                                      message.chat.id
+                                                      )
                 # IF USER BANNED FROM CHANNEL
                 if userStatus.status == 'banned':
-                     await message.reply_photo(
-                         photo = BANNED_PIC, quote = True,
-                         caption = "For Some Reason You Can't Use This Bot"
-                                   "\n\nContact Bot Owner 🤐",
-                         reply_markup = InlineKeyboardMarkup(
-                            [[InlineKeyboardButton("Owner 🎊", url="https://t.me/nabilanavab")]]
-                         )
-                     )
-                     return
+                     return await message.reply_photo(
+                                                     photo = BANNED_PIC, quote = True,
+                                                     caption = "For Some Reason You Can't Use This Bot"
+                                                               "\n\nContact Bot Owner 🤐",
+                                                     reply_markup = InlineKeyboardMarkup(
+                                                            [[InlineKeyboardButton("Owner 🎊", url="https://t.me/nabilanavab")]]
+                                                     ))
             except Exception:
                 if invite_link == None:
-                    invite_link = await bot.create_chat_invite_link(int(UPDATE_CHANNEL))
-                await message.reply_photo(
-                    photo = WELCOME_PIC, quote = True,
-                    caption = forceSubMsg.format(
-                        message.from_user.first_name, message.chat.id
-                    ),
-                    reply_markup = InlineKeyboardMarkup(
-                        [[
-                            InlineKeyboardButton("🌟 JOIN CHANNEL 🌟",
-                                        url = invite_link.invite_link)
-                        ],[
-                            InlineKeyboardButton("Refresh ♻️",
-                                         callback_data = "refreshImg")
-                        ]]
-                    )
-                )
-                return
-        imageReply = await message.reply_text("`Downloading your Image..⏳`", quote=True)
+                    invite_link = await bot.create_chat_invite_link(
+                                                                   int(UPDATE_CHANNEL)
+                                                                   )
+                return await message.reply_photo(
+                                         photo = WELCOME_PIC,
+                                         quote = True,
+                                         caption = forceSubMsg.format(
+                                             message.from_user.first_name, message.chat.id
+                                         ),
+                                         reply_markup = InlineKeyboardMarkup(
+                                             [[
+                                                 InlineKeyboardButton("🌟 JOIN CHANNEL 🌟",
+                                                             url = invite_link.invite_link)
+                                             ],[
+                                                 InlineKeyboardButton("Refresh ♻️",
+                                                              callback_data = "refreshImg")
+                                             ]]
+                                         ))
+        imageReply = await message.reply_text(
+                                             "`Downloading your Image..⏳`",
+                                             quote=True
+                                             )
         if not isinstance(PDF.get(message.chat.id), list):
             PDF[message.chat.id] = []
         await message.download(
-            f"{message.chat.id}/{message.chat.id}.jpg"
-        )
+                              f"{message.chat.id}/{message.chat.id}.jpg"
+                              )
         img = Image.open(
             f"{message.chat.id}/{message.chat.id}.jpg"
         ).convert("RGB")
         PDF[message.chat.id].append(img)
         await imageReply.edit(
-            imageAdded.format(len(PDF[message.chat.id]))
+                             imageAdded.format(
+                                              len(PDF[message.chat.id])
+                                              )
         )
     except Exception as e:
         logger.exception(
