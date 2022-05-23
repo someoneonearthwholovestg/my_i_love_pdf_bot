f70a1aafd9416d9e6f8a821a72a317f8192c4d82
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-05-23 12:05:49 +0530

    Update thumbnail.py

--- plugins/dm/thumbnail.py
+++ plugins/dm/thumbnail.py
@@ -1,4 +1,4 @@
-# fileName : plugins/dm/thumbnail.py
+# fileName : plugins/thumbName.py
 # copyright ©️ 2021 nabilanavab
 
 # LOGGING INFO: DEBUG
@@ -9,218 +9,90 @@ logging.basicConfig(
                    format="%(levelname)s:%(name)s:%(message)s" # %(asctime)s:
                    )
 
-import asyncio
-from pyromod import listen
-from pyrogram import filters
-from plugins.dm.start import _back
+import os
+from PIL import Image
+from pyrogram import Client
+from pyrogram.types import Message
 from configs.db import isMONGOexist
-from pyrogram import Client as ILovePDF
-from pyrogram.types import InputMediaPhoto
+from configs.images import DEFAULT_NAME
+from configs.images import PDF_THUMBNAIL   # DEFAULT THUMBNAIL
 from configs.images import CUSTOM_THUMBNAIL_U
-from configs.images import PDF_THUMBNAIL, WELCOME_PIC
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+# THUMBNAIL METADATA
+from hachoir.parser import createParser
+from hachoir.metadata import extractMetadata
 
 if isMONGOexist:
     from database import db
 
-# NB: lots and lots and lots of time wasted.. 😓
-# https://docs.pyrogram.org/api/methods/edit_message_media
+changeNAME=False
+if DEFAULT_NAME:
+   changeNAME=True
 
-# CUSTOM THUMBNAIL 
-@ILovePDF.on_message(
-                    filters.command("thumbnail") &
-                    filters.private & ~filters.edited
-                    )
-async def _thumbnail(bot, message):
+# return thumbnail height
+async def thumbMeta(thumbPath: str):
     try:
+        metadata = extractMetadata(createParser(thumbPath))
+        if metadata.has("height"):
+            return metadata.get("height")
+        else:
+            return 0
+    except Exception as e:
+        logger.exception(
+                        "THUMB_META:CAUSES %(e)s ERROR",
+                        exc_info=True
+                        )
+
+# photo_id -> local image
+async def formatThumb(location):
+    try:
+        height = await thumbMeta(location)
+        Image.open(thumb_path).convert("RGB").save(location)
+        img = Image.open(location)
+        img.resize((320, height))
+        img.save(location, "JPEG")
+        return location
+    except Exception as e:
+        logger.exception(
+                        "LOCAL_THUMB:CAUSES %(e)s ERROR",
+                        exc_info=True
+                        )
+
+# return thumbnail and fileName
+async def thumbName(message, fileName):
+    try:
+        fileNm, fileExt = os.path.splitext(fileName)
+        if changeNAME:
+            DEFAULT_NAME = DEFAULT_NAME + fileExt
+        
+        # if no mongoDB return False [default thumbnail ]
         if not isMONGOexist:
-            # if No mongoDB Url
-            await message.reply(
-                               "Can't Use This Feature 🤧",
-                               quote = True
-                               )
-            return
-        elif message.reply_to_message and message.reply_to_message.photo:
-            # set thumbnail
-            await db.set_thumbnail(
-                message.from_user.id, message.reply_to_message.photo.file_id
-            )
-            await message.reply_photo(
-                                     photo = message.reply_to_message.photo.file_id,
-                                     caption = "Okay,\n"
-                                              "I will use this image as custom thumbnail.. 🖐️",
-                                     reply_markup = InlineKeyboardMarkup(
-                                              [[InlineKeyboardButton("Delete Thumbnail",
-                                                       callback_data = "delThumb")]]
-                                     ),
-                                     quote = True
-                                     )
-            CUSTOM_THUMBNAIL_U.append(message.chat.id)
-            return
+            # id no DEFAULT_NAME, use current file name 
+            if changeNAME:
+                return PDF_THUMBNAIL, DEFAULT_NAME
+            else:
+                return PDF_THUMBNAIL, fileName
+        
+        # user with thumbnail
+        if message.chat.id in CUSTOM_THUMBNAIL_U:
+            thumbnail = await db.get_thumbnail(message.chat.id)
+            if changeNAME:
+                return thumbLoc, DEFAULT_NAME
+            else:
+                return thumbnail, fileName
+        
+        # user without thumbnail
         else:
-            if message.chat.id not in CUSTOM_THUMBNAIL_U:
-                await message.reply(
-                                    "You didn't set custom thumbnail!\n"
-                                    "reply /thumbnail to set thumbnail",
-                                    quote = True
-                                    )
-                return
-            # Get Thumbnail from DB
-            thumbnail = await db.get_thumbnail(message.from_user.id)
-            if not thumbnail:
-                await message.reply(
-                                    "You didn't set custom thumbnail!\n"
-                                    "reply /thumbnail to set thumbnail",
-                                    quote = True
-                                    )
-                return
-            await message.reply_photo(
-                                     photo = thumbnail,
-                                     caption = "Custom Thumbnail",
-                                     quote = True,
-                                     reply_markup = InlineKeyboardMarkup(
-                                            [[InlineKeyboardButton("Delete Thumbnail",
-                                                   callback_data = "delThumb")]]
-                                     ))
-            return
+            if changeNAME:
+                return PDF_THUMBNAIL, DEFAULT_NAME
+            else:
+                return PDF_THUMBNAIL, fileName
+    
     except Exception as e:
         logger.exception(
-                        "/THUMBNAIL:CAUSES %(e)s ERROR",
+                        "THUMB_NAME:CAUSES %(e)s ERROR",
                         exc_info=True
                         )
 
 
-geThumb = filters.create(lambda _, __, query: query.data=="getThumb")
-addThumb = filters.create(lambda _, __, query: query.data=="addThumb")
-delThumb = filters.create(lambda _, __, query: query.data=="delThumb")
-
-
-@ILovePDF.on_callback_query(geThumb)
-async def _getThumb(bot, callbackQuery):
-    try:
-        if not isMONGOexist:
-            await callbackQuery.answer(
-                                      "Can't Use This Feature 🤧"
-                                      )
-            return
-        else:
-            await callbackQuery.answer(
-                                      "wait.! Let me think.. 🤔"
-                                      )
-            thumbnail=await db.get_thumbnail(callbackQuery.message.chat.id)
-            if not thumbnail:
-                try:
-                    await callbackQuery.edit_message_media(InputMediaPhoto(PDF_THUMBNAIL))
-                except Exception:
-                    pass
-                await callbackQuery.edit_message_caption(
-                                                        caption="🌟 CURRENT THUMBNAIL 🌟 (DEFAULT)\n\n"
-                                                                "You didn't set any custom thumbnail!\n\n"
-                                                                "/thumbnail :\n◍ To get current thumbnail\n"
-                                                                "◍ Reply to a photo to set custom thumbnail",
-                                                        reply_markup=InlineKeyboardMarkup(
-                                                            [[InlineKeyboardButton("😒 ADD THUMB 😒",
-                                                                     callback_data="addThumb")],
-                                                             [InlineKeyboardButton("« BACK «",
-                                                                     callback_data="back")]]
-                                                        ))
-                return
-            await callbackQuery.edit_message_media(InputMediaPhoto(thumbnail))
-            await callbackQuery.edit_message_caption(
-                                                    caption="🌟 CURRENT THUMBNAIL 🌟\n\n"
-                                                            "/thumbnail :\n◍ To get current thumbnail\n"
-                                                            "◍ Reply to a photo to set custom thumbnail",
-                                                    reply_markup=InlineKeyboardMarkup(
-                                                        [[InlineKeyboardButton("🥲 CHANGE 🥲",
-                                                                 callback_data="addThumb"),
-                                                          InlineKeyboardButton("🤩 DELETE 🤩",
-                                                                 callback_data="delThumb")],
-                                                         [InlineKeyboardButton("« BACK «",
-                                                                 callback_data="back")]]
-                                                    ))
-            return
-    except Exception as e:
-        logger.exception(
-                        "GET_THUMB:CAUSES %(e)s ERROR",
-                        exc_info=True
-                        )
-
-
-@ILovePDF.on_callback_query(addThumb)
-async def _addThumb(bot, callbackQuery):
-    try:
-        await callbackQuery.answer()
-        await callbackQuery.edit_message_caption(
-                                                caption="Now, Send me a Image..",
-                                                reply_markup=InlineKeyboardMarkup(
-                                                    [[InlineKeyboardButton("Waiting.. 🥱",
-                                                             callback_data="noResponse")]]
-                                                ))
-        await asyncio.sleep(1)
-        await callbackQuery.edit_message_caption(
-                                                caption="Now, Send me a Image for Future Use.. 😅\n\n"
-                                                        "Don't have enough time, send me fast 😏",
-                                                reply_markup=InlineKeyboardMarkup(
-                                                    [[InlineKeyboardButton("Waiting.. 🥱",
-                                                             callback_data="noResponse")]]
-                                                ))
-        getThumb=await bot.listen(callbackQuery.from_user.id)
-        if not getThumb.photo:
-            await getThumb.delete()
-            await _back(bot, callbackQuery)
-        else:
-            await callbackQuery.edit_message_media(InputMediaPhoto(getThumb.photo.file_id))
-            await callbackQuery.edit_message_caption(
-                                                    caption="🌟 CURRENT THUMBNAIL 🌟\n\n"
-                                                            "/thumbnail :\n◍ To get current thumbnail\n"
-                                                            "◍ Reply to a photo to set custom thumbnail",
-                                                    reply_markup=InlineKeyboardMarkup(
-                                                        [[InlineKeyboardButton("🥲 CHANGE 🥲",
-                                                                 callback_data="addThumb"),
-                                                          InlineKeyboardButton("🤩 DELETE 🤩",
-                                                                 callback_data="delThumb")],
-                                                         [InlineKeyboardButton("« BACK «",
-                                                                 callback_data="back")]]
-                                                    ))
-            await db.set_thumbnail(
-                                  callbackQuery.from_user.id,
-                                  getThumb.photo.file_id
-                                  )
-            await getThumb.delete()
-            CUSTOM_THUMBNAIL_U.append(
-                                     callbackQuery.message.chat.id
-                                     )
-    except Exception as e:
-        logger.exception(
-                        "ADD_THUMB:CAUSES %(e)s ERROR",
-                        exc_info=True
-                        )
-
-
-@ILovePDF.on_callback_query(delThumb)
-async def _delThumb(bot, callbackQuery):
-    try:
-        if callbackQuery.message.chat.id in CUSTOM_THUMBNAIL_U:
-            return await callbackQuery.answer(
-                                             "Already Banned.. 🤧"
-                                             )
-        await callbackQuery.answer(
-                                  "Deleted.. 😎"
-                                  )
-        await callbackQuery.edit_message_media(InputMediaPhoto(WELCOME_PIC))
-        await _back(bot, callbackQuery)
-        await db.set_thumbnail(
-                              callbackQuery.from_user.id,
-                              None
-                              )
-        CUSTOM_THUMBNAIL_U.remove(
-                                 callbackQuery.message.chat.id
-                                 )
-    except Exception as e:
-        logger.exception(
-                        "DEL_THUMB:CAUSES %(e)s ERROR",
-                        exc_info=True
-                        )
-
-
-#                                                                                        Telegram: @nabilanavab
+#                                                         Telegram: @nabilanavab
