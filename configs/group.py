76e3ea709c0764a2ef57d30f8f5f13eddb62fcc3
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-05-24 15:23:22 +0530

    Update group.py

--- configs/group.py
+++ configs/group.py
@@ -10,7 +10,7 @@ import os
 class groupConfig(object):
     
     # add admins Id list by space seperated (Optional)
-    ONLY_GROUP=list(set(int(x) for x in os.environ.get("ONLY_GROUP", "0").split()))
+    ONLY_GROUP=list(set(int(x) for x in os.environ.get("ONLY_GROUP", False).split()))
     
     # banned groups can't use this bot (Optional)
     BANNED_GROUP=list(set(int(x) for x in os.environ.get("BANNED_USERS", "0").split()))
