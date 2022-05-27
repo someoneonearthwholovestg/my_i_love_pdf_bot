6d2d83e274a311896dfc3aa8c2b079c4a964df33
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-05-27 23:29:19 +0530

    Update generate.py

--- plugins/dm/generate.py
+++ plugins/dm/generate.py
@@ -8,6 +8,7 @@ logging.basicConfig(
                    level=logging.INFO,
                    format="%(levelname)s:%(name)s:%(message)s" # %(asctime)s:
                    )
+logging.getLogger("pillow").setLevel(logging.ERROR)
 
 import os
 import shutil
