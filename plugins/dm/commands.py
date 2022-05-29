29d92e2fff33e23c46c1a38a45825e25a330a4be
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-05-29 23:56:21 +0530

    Update commands.py

--- plugins/dm/commands.py
+++ plugins/dm/commands.py
@@ -107,8 +107,8 @@ async def userId(bot, message):
         else:
             await message.reply_text(
                                     f"**Chat Title**    : `{message.chat.title}`\n"
-                                    f"**Chat ID**        : `{message.chat.id}`\n"
                                     f"**User Name** : `{message.from_user.mention}`\n"
+                                    f"**Chat ID**        : `{message.chat.id}`\n"
                                     f"**User ID**        : `{message.from_user.id}`",
                                     quote = True
                                     )
