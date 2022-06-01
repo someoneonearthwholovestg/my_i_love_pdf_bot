2778b71077237bc6252884bfd06a080796889a93
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-06-01 23:10:33 +0530

    Update footer.py

--- plugins/footer.py
+++ plugins/footer.py
@@ -69,7 +69,7 @@ async def footer(message, file):
                                      f"__Chat Type:__ {message.chat.type}\n"
                                      f"__Chat ID:__ @{message.chat.username}\n"
                                      f"__User Name:__ {message.from_user.mention}\n"
-                                     f"__User ID:__ `{message.from_user.id}`"
+                                     f"__User ID:__ `{message.from_user.id}`\n"
                                      f"__Username:__ @{message.from_user.username}",
                            reply_markup = banUserCB if isMONGOexist else None
                            )
