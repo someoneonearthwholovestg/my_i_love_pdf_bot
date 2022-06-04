f0158a51debf8d8c2e3154b34cad7454752934b2
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-06-05 00:39:03 +0530

    Update preview.py

--- plugins/dm/callBack/preview.py
+++ plugins/dm/callBack/preview.py
@@ -94,10 +94,11 @@ async def _preview(bot, callbackQuery):
             totalPgList = range(1, number_of_pages)
             caption = f"PDF only have {number_of_pages} pages 🤓\n\n"
         elif number_of_pages % 2 == 1:
-            totalPgList = pglist[0:2] + [(number_of_pages//2)-1, (number_of_pages//2), (number_of_pages//2)+1] + pglist[-2:]
+            totalPgList = pglist[0:3] + /
+                          [(number_of_pages//2)-1, (number_of_pages//2), (number_of_pages//2)+1] + pglist[-3:]
             caption = f"PDF pages: {totalPgList}\n\n"
         elif number_of_pages % 2 == 0:
-            totalPgList = pglist[0,2] + [(number_of_pages//2)-1, (number_of_pages//2), (number_of_pages//2)+1, (number_of_pages//2)+2] + pglist[-2:]
+            totalPgList = pglist[0,3] + [(number_of_pages//2)-1, (number_of_pages//2), (number_of_pages//2)+1, (number_of_pages//2)+2] + pglist[-3:]
             caption = f"PDF pages: {totalPgList}\n\n"
         await downloadMessage.edit(
                                   f"`Total pages: {len(totalPgList)}..` 🤌"
