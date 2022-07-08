686fd55aaaa6a0856d3e5ef42c62ab060b647338
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2022-07-08 21:16:14 +0530

    Update _main_.py

--- plugins/dm/callBack/_main_.py
+++ plugins/dm/callBack/_main_.py
@@ -243,7 +243,7 @@ async def _pdf(bot, callbackQuery):
                                   )
         # CHECK PDF OR NOT(HERE compressed, SO PG UNKNOWN)
         if (data.startswith("Kdecrypt")) or (data[0] != 'K') or not (data in ["rot180", "rot90", "rot270"]):
-            # check file encryption, codec.
+            await downloadMessage.edit("entered") # check file encryption, codec.
             checked, number_of_pages = await checkPdf(input_file, callbackQuery)
             if data.startswith(tuple(["decrypt", "Kdecrypt"])):
                 if not(checked == "encrypted"):
